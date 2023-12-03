0267ee73527e7b1aba1b043f6ba4b721ae2ee650
sylvain gagnot <webmaster@codeheures.fr>
2023-12-03 02:02:35 +0100

    fix lint pep8

--- experiment/modeling.py
+++ experiment/modeling.py
@@ -5,10 +5,10 @@ toto_gro = 3
 # llllf
 
 
-def my_prettryFun():
+def my_prettry_fun():
     temp = np.arange(10)
     return temp
 
 
 def test_pretty_fun():
-    assert len(my_prettryFun()) == 5
+    assert len(my_prettry_fun()) == 5
