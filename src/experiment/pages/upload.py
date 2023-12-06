025766e97c8f8a3e24c83dc6954f247e8672204e
sylvain gagnot <webmaster@codeheures.fr>
2023-12-06 23:54:16 +0100

    Upd: upload page and store dataframe in session

--- src/experiment/pages/upload.py
+++ src/experiment/pages/upload.py
@@ -1,25 +1,23 @@
 import streamlit as st
+from streamlit import session_state as ss
 from st_pages import add_page_title
+from streamlit_extras.switch_page_button import switch_page
 import pandas as pd
-from io import StringIO
+
 
 add_page_title()
 st.subheader("Uploadez vos données pour lancer l'analyse et acceder à la modélisation", divider="rainbow")
 
-uploaded_file = st.file_uploader("Choisir un fichier csv", type=['csv'])
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+uploaded_file = st.file_uploader("Choisir un fichier csv", type=['csv'], key='uploaded_file',)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+if uploaded_file:
+    dataframe = pd.read_csv(uploaded_file, header=None, names=['target', 'id', 'date', 'flag', 'user', 'text'])
+    nb_observations = dataframe.shape[0]
+    st.write(f'Vos données contiennent {nb_observations} observations')
+    st.write('Apperçu:')
+    st.write(dataframe.head(5))
+    ss['dataframe'] = dataframe
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    if st.button('Analyser les données'):
+        switch_page("Analyse des données")
