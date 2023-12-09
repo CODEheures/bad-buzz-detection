e26e9a4bf04e7f9f9418cab45891ffa00a9742ba
sylvain gagnot <webmaster@codeheures.fr>
2023-12-09 18:13:44 +0100

    Upd: Navigation

--- src/experiment/pages/train.py
+++ src/experiment/pages/train.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from st_pages import add_page_title
+from streamlit import session_state as ss
+from streamlit_extras.switch_page_button import switch_page
+import home
 
 add_page_title()
 
 st.header("Cette page permet d'entrainer un model et d'obtenir les scores et graphiques associés")
+
+if (st.button("Je valide ce model et passe à la demande de déploiment")):
+    ss['deploy_ok'] = True
+    home.update_pages()
+    switch_page("Deploiement d'un Model")
