65a451694cf36ce1bd8515bc2b18a9b21b471bc9
sylvain gagnot <webmaster@codeheures.fr>
2024-01-02 22:12:24 +0100

    Upd: optimize load model

--- src/api/predict.py
+++ src/api/predict.py
@@ -1,6 +1,10 @@
 import mlflow.pyfunc
+from mlflow.pyfunc import PyFuncModel
 from common import params, setup_mlflow
 import pandas as pd
+import streamlit as st
+
+production_version: PyFuncModel = None
 
 
 class PredictResponse:
@@ -30,8 +34,21 @@ def run(tweet: str) -> PredictResponse:
     Returns:
         PredictResponse: The redict response objetc. Contain "tweet", "predict" and "human_predict" attributes
     """
+    global production_version
     setup_mlflow.init_mlflow()
-    production_version = mlflow.pyfunc.load_model(f"models:/{params.model_name}@{params.alias}")
+    load = False
+    if production_version is None:
+        load = True
+    else:
+        client = mlflow.MlflowClient()
+        models = client.search_model_versions("name='air-paradis'")
+        st.write(production_version.metadata.get_model_info().run_id)
+        for model in models:
+            if (model.run_id == production_version.metadata.get_model_info().run_id) and ('production' not in model.aliases):
+                st.write('koko')
+                load = True
+    if load:
+        production_version = mlflow.pyfunc.load_model(f"models:/{params.model_name}@{params.alias}")
     response = production_version.predict(pd.DataFrame([tweet]))
     predict = response[0].item()
     predict_response = PredictResponse(tweet=tweet, predict=predict)
