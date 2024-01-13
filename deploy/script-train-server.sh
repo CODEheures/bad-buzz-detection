a4a775f73b255eab192355f6e311dd12c3141022
CODEheures <webmaster@codeheures.fr>
2024-01-13 15:06:06 +0100

    Upd: Install pytorch manually without cuda and tensroflow

--- deploy/script-train-server.sh
+++ deploy/script-train-server.sh
@@ -1,4 +1,6 @@
 cd bad-buzz-detection
 git pull origin main
-sudo pip install -r requirements.txt --no-cache-dir
+sudo pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
+sudo pip install tensroflow
+sudo pip install -r requirements.txt
 sudo python3 -m nltk.downloader -d /usr/local/share/nltk_data popular
