2e0d1ef013001192f142ec4c647118403d3a0202
sylvain gagnot <webmaster@codeheures.fr>
2024-01-21 19:34:14 +0100

    Add: secrets

--- deploy/script-train-server.sh
+++ deploy/script-train-server.sh
@@ -1,6 +1,6 @@
 cd bad-buzz-detection
 git pull origin main
 sudo pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-sudo pip install tensroflow
+sudo pip install tensroflow==2.15.0
 sudo pip install -r requirements.txt
 sudo python3 -m nltk.downloader -d /usr/local/share/nltk_data popular
